b82401ad529b3ba64097b866b156149bcbd4916d
agusgarone <agusgarone02@gmail.com>
2025-10-28 15:35:11 -0300

    FEAT: Add Dotenv for credentials

--- models/users.js
+++ models/users.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
-const uri =
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
